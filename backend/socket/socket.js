6cc4fe448b0c0fe0622b58dbf3d6f180201ee909
nitinjha199 <nitinkumarjha199@gmail.com>
2025-06-27 23:57:13 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,7 @@ let app=express()
 const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:5173",
+        origin:"https://chat-on-adgw.onrender.com",
         // credentials:true
     }
 })

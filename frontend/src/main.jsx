cec3a9c048f5da9d691655f7856e3e41842c0ab9
nitinjha199 <nitinkumarjha199@gmail.com>
2025-06-28 01:28:12 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
+import axios from 'axios';
 import {Provider} from "react-redux"
 import { store } from './redux/store.js'
+axios.defaults.withCredentials = true;
 export const serverURL="https://chat-on-backend-j3cp.onrender.com"
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>

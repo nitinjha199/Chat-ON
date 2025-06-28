85b891652edd5e1ae2c763785af0991c357fab2a
nitinjha199 <nitinkumarjha199@gmail.com>
2025-06-28 09:22:13 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -68,11 +68,16 @@ export const login= async (req,res)=>{
  
 }
 
-export const logOut=async (req,res)=>{
-    try {
-        res.clearCookie("token")
-        return res.status(200).json({message:"logout successfully"})
-    } catch (error) {
-        return res.status(500).json({message:"logout error"})
-    }
-}
+export const logOut = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+    });
+    return res.status(200).json({ message: "logout successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "logout error" });
+  }
+};
+
